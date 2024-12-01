66240eca3190a2914018578317f7a7a5954ee5ac
kjabin <185402721+kjabin@users.noreply.github.com>
2024-12-01 19:35:24 +0530

    add repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,14 +8,17 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic("???")
+	// infinite repl
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+
+		// Wait for user input
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic("???")
+		}
+		cmd = strings.TrimRight(cmd, "\n")
+		fmt.Printf("%v: command not found\n", cmd)
 	}
-	cmd = strings.TrimRight(cmd, "\n")
-	fmt.Printf("%v: command not found\n", cmd)
 }
